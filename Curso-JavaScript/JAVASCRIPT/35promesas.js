8eb5e92422d4380e17b8fac20bd94b57c31c6461
FLotito91 <--global user.email federicojl1991@gmail.com>
2023-03-02 21:36:27 -0300

    modificando archivos en general

--- Curso-JavaScript/JAVASCRIPT/35promesas.js
+++ Curso-JavaScript/JAVASCRIPT/35promesas.js
@@ -27,4 +27,28 @@ let promesa = new Promise((resolver) => {
 async function miFuncionConPromesa(){
     return 'saludos con promesa y async';
 };
-miFuncionConPromesa().then(valor => console.log(valor));
+//miFuncionConPromesa().then(valor => console.log(valor));
+
+// async/await
+async function funcionConPromesaYAwait(){
+    let miPromesa = new Promise((resolver) => {
+        resolver ("Promesa con await")
+    })
+    console.log(await miPromesa);
+}
+
+//funcionConPromesaYAwait();
+
+//promesas, await, async y setTimeout
+async function funcionConPromesaAwaiTimeout(){
+    console.log("inicio de función")
+    let miPromesa = new Promise(resolver => {
+        setTimeout(()=>resolver("Promesa con await y time out", 3000));
+    });
+    console.log(await miPromesa);
+    console.log("fin de la función")
+}
+
+funcionConPromesaAwaiTimeout()
+
+//await solo puede ser llamada mediante una función async. 
